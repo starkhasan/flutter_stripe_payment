5a3e17a1316a85ddcc6bbcf64e63b52d5650dfe4
starkhasan <alihasan226@gmail.com>
2023-07-15 19:21:07 +0530

    Update UI

--- lib/payment_screen.dart
+++ lib/payment_screen.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:flutter_stripe/flutter_stripe.dart';
 import 'package:http/http.dart';
@@ -13,36 +14,115 @@ class PaymentScreen extends StatefulWidget {
 class _PaymentScreenState extends State<PaymentScreen> {
 
   var paymentIntent = {};
-
+  var item = 4;
+  var totalAmount = 0;
+  
+  @override
+  void initState() {
+    totalAmount = item * 125;
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(centerTitle: true, title: const Text('Payment')),
-      body: Container(),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () => makePayment(),
-        child: const Icon(Icons.download),
+      appBar: AppBar(
+        centerTitle: true, title: const Text('Proceed to payment'),
+        actions: [
+          IconButton(
+            onPressed: () => addNewProduct(), 
+            icon: const Icon(Icons.add)
+          )
+        ],
       ),
+      body: Column(
+        children: [
+          Expanded(
+            child: ListView.builder(
+              shrinkWrap: true,
+              itemCount: item,
+              itemBuilder: (context, index){
+                return Container(
+                  padding: const EdgeInsets.symmetric(vertical: 20.0, horizontal: 20.0),
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Container(
+                        height: 100,
+                        width: 100,
+                        color: Colors.primaries[Random().nextInt(Colors.primaries.length)]
+                      ),
+                      const SizedBox(width: 20.0),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: const [
+                          Text('Product Name'),
+                          SizedBox(height: 20.0),
+                          Text('125')
+                        ],
+                      )
+                    ],
+                  ),
+                );
+              }
+            ),
+          ),
+          const Divider(height: 1.0,thickness: 1.0,color: Colors.grey),
+          Container(
+            padding: const EdgeInsets.symmetric(horizontal: 15.0, vertical: 15.0),
+            child: Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    const Text('Total Amount'),
+                    Text(totalAmount.toString())
+                  ],
+                ),
+                const SizedBox(height: 15.0),
+                SizedBox(
+                  width: double.maxFinite,
+                  child: ElevatedButton(
+                    onPressed: () => makePayment(), 
+                    style: ElevatedButton.styleFrom(
+                      shape: const RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(10.0)))
+                    ),
+                    child: const Text('Pay'),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+      // floatingActionButton: FloatingActionButton(
+      //   onPressed: () => makePayment(),
+      //   child: const Icon(Icons.download),
+      // ),
     );
   }
+
+  void addNewProduct() {
+    item+=1;
+    totalAmount = 125 * item;
+    update();
+  }
+
+  void update() => setState(() {});
 
   ///Make Payment Function
   Future<void> makePayment() async {
     try {
       //STEP 1: Create Payment Intent
-      paymentIntent = await createPaymentIntent('100', 'INR');
+      paymentIntent = await createPaymentIntent(totalAmount, 'INR');
       //STEP 2: Initialize Payment Sheet
-      await Stripe.instance
-          .initPaymentSheet(
-            
-              paymentSheetParameters: SetupPaymentSheetParameters(
-                  paymentIntentClientSecret: paymentIntent[
-                      'client_secret'], //Gotten from payment intent
-                  style: ThemeMode.light,
-                  merchantDisplayName: 'Ikay'))
-          .then((value) {});
-
+      await Stripe.instance.initPaymentSheet(
+        paymentSheetParameters: SetupPaymentSheetParameters(
+          paymentIntentClientSecret: paymentIntent['client_secret'], //Gotten from payment intent
+          style: ThemeMode.light,
+          merchantDisplayName: 'Ikay'
+        )
+      ).then((value) {});
       //STEP 3: Display Payment sheet
       displayPaymentSheet();
     } catch (err) {
@@ -51,13 +131,13 @@ class _PaymentScreenState extends State<PaymentScreen> {
   }
 
 
-  String calculateAmount(String amount){
-    var convertAmount = int.parse(amount) * 100;
+  String calculateAmount(int amount){
+    var convertAmount = amount * 100;
     return convertAmount.toString();
   }
 
   ///Create Payment Intent
-  createPaymentIntent(String amount, String currency) async {
+  createPaymentIntent(int amount, String currency) async {
     try {
       //Request body
       Map<String, dynamic> body = {
